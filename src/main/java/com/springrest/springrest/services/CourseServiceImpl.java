3a61ac8e83065803da9b61f50c45e1891ec7f639
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-08-03 10:41:22 +0530

    String of API data to database in spring

--- src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -4,60 +4,69 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
-	List<Course> list;
+//	List<Course> list;
+	
+	@Autowired
+	private CourseDao courseDao;
 	
 	public CourseServiceImpl() {
-		list = new ArrayList<>();
-		list.add(new Course(101, "Java Core Course", "This is java Course"));
-		list.add(new Course(102, "Spring Boot Course", "This is Spring Boot"));
+//		list = new ArrayList<>();
+//		list.add(new Course(101, "Java Core Course", "This is java Course"));
+//		list.add(new Course(102, "Spring Boot Course", "This is Spring Boot"));
 		
 	}
 	
 	@Override
 	public List<Course> getCourses() {
 		// TODO Auto-generated method stub
-		return list;
+		return courseDao.findAll();
 	}
 
 	@Override
 	public Course getCourse(long courseId) {
-		Course c = null;
-		for(Course course : list) {
-			if(course.getId() == courseId) {
-				c=course;
-				break;
-			}
-		}
-		return c;
+//		Course c = null;
+//		for(Course course : list) {
+//			if(course.getId() == courseId) {
+//				c=course;
+//				break;
+//			}
+//		}
+		return courseDao.getOne(courseId);
 	}
 
 	@Override
 	public Course addCourse(Course course) {
-		list.add(course);
+//		list.add(course);
+		courseDao.save(course);
 		return course;
 	}
 
 	@Override
 	public Course updateCourse(Course course) {
-		list.forEach(e -> {
-			if((e.getId() == course.getId())) {
-				e.setTitle(course.getTitle());
-				e.setDescription(course.getDescription());
-			}
-		});
+//		list.forEach(e -> {
+//			if((e.getId() == course.getId())) {
+//				e.setTitle(course.getTitle());
+//				e.setDescription(course.getDescription());
+//			}
+//		});
+		courseDao.save(course);
 		return course;
 	}
 
 	@Override
 	public void deleteCourse(long parseLong) {
-		list = this.list.stream().filter(e -> e.getId()!=parseLong).collect(Collectors.toList());
+//		list = this.list.stream().filter(e -> e.getId()!=parseLong).collect(Collectors.toList());
+		Course entity = courseDao.getOne(parseLong);
+		courseDao.delete(entity);
 	}
 
 }
